90aab025221ac22326fc571f50a25d9aae16049d
bhuang02 <105864196+bhuang02@users.noreply.github.com>
2025-12-01 08:29:38 +0100

    Fix syntax error for first change

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
   echo "Hello from Azure Web App with CI/CD!<br>";
   echo "Deployed via GitHub + Deployment Center.";
-  echo "This is the first change that I will make to test CI/CD":
+  echo "This is the first change that I will make to test CI/CD";
 ?>
